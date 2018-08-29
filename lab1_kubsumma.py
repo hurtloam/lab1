db5842aa0d16a4939a381065c0f5e1f13294658a
peter <peter.brorsson@brorssonsite.com>
2018-08-29 23:09:37 +0200

    Finds all up to 20683

--- lab1_kubsumma.py
+++ lab1_kubsumma.py
@@ -1,18 +1,18 @@
+def findXY(z):
+    lower = 1
+    upper = 50
+    successList = []
+    for y in range(lower,upper):
+        for x in range(lower,upper):
+            v = x**3 + y**3
+            if (v > z):
+                break
+            if (v == z):
+                successList.append([z,x,y])
+                if (len(successList) == 4):
+                    for s in successList[0:2]:
+                        print "For z = %d: x = %d and y = %d" % (s[0], s[1], s[2])
+                break
 
-z = 1729
-lower = 1
-upper = 15
-
-for 
-
-for y in range(lower,upper):
-    for x in range(lower,upper):
-        v = x**3 + y**3
-        if (v > z):
-            break
-        if (v == z):
-            print "Values of x and y: x = %d and y = %d" % (x, y)
-            break
-    
-
-
+for z in range (1,30000):
+    findXY(z)
