e05bd7c5d044b3a66baba01b72761ec9acb083d6
peter <peter.brorsson@brorssonsite.com>
2018-08-29 22:01:01 +0200

    finshed lab1

--- lab1_kubsumma.py
+++ lab1_kubsumma.py
@@ -1,4 +1,18 @@
-print (1+2)
-En nyrad
-en till rad
-En tredje rad
+
+z = 1729
+lower = 1
+upper = 15
+
+for 
+
+for y in range(lower,upper):
+    for x in range(lower,upper):
+        v = x**3 + y**3
+        if (v > z):
+            break
+        if (v == z):
+            print "Values of x and y: x = %d and y = %d" % (x, y)
+            break
+    
+
+
